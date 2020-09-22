d5ddfb2ed97520e2b33be4cafbff67cf21ff7487
Glitch (brianburke96-rockpaperscissors) <none>
2020-09-22 00:07:43 +0000

    Import of brianburke96/rockpaperscissors

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// *************************** YOUR CODE BELOW *******************************
+//******************TEST EARLY AND OFTEN USING console.log() ******************
+//****************** SERIOUSLY TEST USING console.log()!!! ******************
+
+//GLOBAL VARIABLES
+/* global $ */
+
+
+
